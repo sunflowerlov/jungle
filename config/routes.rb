3634c95142053cfe825613dcb58e124b68f1d46d
Lisha Liang <lishaliang@Lishas-MacBook-Pro.local>
2023-04-22 11:34:42 -0400

    Admin Categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  
+  get 'about/index'
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
@@ -15,6 +17,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
